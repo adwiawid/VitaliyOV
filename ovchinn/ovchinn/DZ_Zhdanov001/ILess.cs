888f2dd614eb0c5fd71d3ccbcf762c3cb7aaa8c7
adwiawid <76702875+adwiawid@users.noreply.github.com>
2022-05-04 21:13:44 +0300

    Add files via upload

---
+++ ovchinn/ovchinn/DZ_Zhdanov001/ILess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovchinn
+{
+    interface ILess
+
+    {
+        void Demo();
+
+        
+        void Info();
+
+       
+        string ID();
+    }
+}
---
+++ ovchinn/ovchinn/DZ_Zhdanov001/Logger.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ovchinn
+{
+    class Logger
+    {
+        public void LoggerStart()
+        {
+           
+            var builder = new ConfigurationBuilder();
+            IConfiguration configuration = BuildConfig(builder);
+            
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+            
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+       
+
+        static IConfiguration BuildConfig(IConfigurationBuilder builder)
+        {
+            return
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appconfig.json", optional: false, reloadOnChange: true)
+            .Build();
+        }
+
+       
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception unhandledException = (Exception)e.ExceptionObject;
+            Log.Error(unhandledException, "Необработанное исключение");
+        }
+    }
+}
